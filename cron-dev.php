bac1362fa73ae3f9f61f520b0a0ce24b6860e5e1
dedanirungu <dedanirungu@gmail.com>
2017-06-19 13:47:52 +0300

    Changes to Cron Script

--- cron-dev.php
+++ cron-dev.php
@@ -12,6 +12,8 @@ $sc = require_once __DIR__ . '/include/container.php';
 $request = Request::createFromGlobals();
 $sc->register('request', $request);
 
+$sc->get('framework')->handle($request);
+
 $cron = new Cron($sc, $request);
 $cron->processCron();
 
